187b4e0e0e287335846976cb4bddf5336a5befc2
timtoronto634 <tim225x@gmail.com>
2023-05-12 08:51:59 +0900

    Feat add upload button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
+import { UploadButton } from './UploadButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Album Viewer</h1>
+      <UploadButton />
     </div>
   );
 }
---
+++ frontend/src/UploadButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UploadButton.css";
+
+function UploadButton() {
+  return (<button className="upload-button">upload</button>);
+}
+
+export { UploadButton };
