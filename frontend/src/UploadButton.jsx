6b445749c86db1ccda2f795f88dd89ec888ba7a0
timtoronto634 <tim225x@gmail.com>
2023-05-12 09:57:07 +0900

    Feat add file input and style

--- frontend/src/UploadButton.jsx
+++ frontend/src/UploadButton.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./UploadButton.css";
 
 function UploadButton() {
-  return (<button className="upload-button">upload</button>);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    console.log('アップロードするファイル: ', file.name);
+  };
+
+  return (
+    <div>
+      <label>
+        <input
+          type="file"
+          onChange={handleFileChange}
+          style={{ display: 'none' }}
+          className="upload-button"
+        />
+        <label htmlFor="fileInput">
+          <button type="button">ファイルをアップロード</button>
+        </label>
+      </label>
+    </div>
+  );
 }
 
 export { UploadButton };
