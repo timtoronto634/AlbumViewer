c3e26e910dc6c418aed1adf5fc310d246e813a69
timtoronto634 <tim225x@gmail.com>
2023-06-07 08:54:52 +0900

    Feat add upload styled button from ant-d
    https://ant.design/components/upload

--- frontend/src/UploadButton.jsx
+++ frontend/src/UploadButton.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./UploadButton.css";
 
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload } from 'antd';
+
 function UploadButton() {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -9,22 +12,63 @@ function UploadButton() {
 
     console.log('アップロードするファイル: ', file.name);
   };
-
+const fileList = [
+  {
+    uid: '0',
+    name: 'xxx.png',
+    status: 'uploading',
+    percent: 33,
+  },
+  {
+    uid: '-1',
+    name: 'yyy.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  },
+  {
+    uid: '-2',
+    name: 'zzz.png',
+    status: 'error',
+  },
+];
+  
   return (
-    <div>
-      <label>
-        <input
-          type="file"
-          onChange={handleFileChange}
-          style={{ display: 'none' }}
-          className="upload-button"
-        />
-        <label htmlFor="fileInput">
-          <button type="button">ファイルをアップロード</button>
-        </label>
-      </label>
-    </div>
+    <>
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture"
+      defaultFileList={[...fileList]}
+    >
+      <Button icon={<UploadOutlined />}>Upload</Button>
+    </Upload>
+  </>
   );
+
+  // return (
+  //   <div>
+  //     <Space
+  //   direction="vertical"
+  //   style={{
+  //     width: '100%',
+  //   }}
+  // >
+  //     <label>
+  //       <input
+  //         type="file"
+  //         id="fileInput"
+  //         onChange={handleFileChange}
+  //         // style={{ display: 'none' }}
+  //         className="upload-button"
+  //       />
+  //       <label htmlFor="fileInput">
+  //         {/* <Button type="primary" block>ファイルをアップロード</Button> */}
+  //         <button type="button">ファイルをアップロード</button>
+  //       </label>
+  //     </label>
+  //     </Space>
+  //   </div>
+  // );
 }
 
 export { UploadButton };
