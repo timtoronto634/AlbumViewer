ee7d9e5b8a241ad5dda74e4749498342e6fb44a5
timtoronto634 <tim225x@gmail.com>
2023-06-14 08:20:24 +0900

    chore: removed css file

--- frontend/src/UploadButton.jsx
+++ frontend/src/UploadButton.jsx
@@ -1,37 +1,29 @@
 import React from "react";
-import "./UploadButton.css";
 
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Upload } from 'antd';
 
 function UploadButton() {
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-
-    if (!file) return;
-
-    console.log('アップロードするファイル: ', file.name);
-  };
-const fileList = [
-  {
-    uid: '0',
-    name: 'xxx.png',
-    status: 'uploading',
-    percent: 33,
-  },
-  {
-    uid: '-1',
-    name: 'yyy.png',
-    status: 'done',
-    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  },
-  {
-    uid: '-2',
-    name: 'zzz.png',
-    status: 'error',
-  },
-];
+  const fileList = [
+    {
+      uid: '0',
+      name: 'xxx.png',
+      status: 'uploading',
+      percent: 33,
+    },
+    {
+      uid: '-1',
+      name: 'yyy.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+    {
+      uid: '-2',
+      name: 'zzz.png',
+      status: 'error',
+    },
+  ];
   
   return (
     <>
@@ -44,31 +36,6 @@ const fileList = [
     </Upload>
   </>
   );
-
-  // return (
-  //   <div>
-  //     <Space
-  //   direction="vertical"
-  //   style={{
-  //     width: '100%',
-  //   }}
-  // >
-  //     <label>
-  //       <input
-  //         type="file"
-  //         id="fileInput"
-  //         onChange={handleFileChange}
-  //         // style={{ display: 'none' }}
-  //         className="upload-button"
-  //       />
-  //       <label htmlFor="fileInput">
-  //         {/* <Button type="primary" block>ファイルをアップロード</Button> */}
-  //         <button type="button">ファイルをアップロード</button>
-  //       </label>
-  //     </label>
-  //     </Space>
-  //   </div>
-  // );
 }
 
 export { UploadButton };
